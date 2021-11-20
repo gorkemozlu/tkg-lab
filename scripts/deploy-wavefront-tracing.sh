cd914492fcab2b9e8e0e8755cbecb47ba2912588
Gorkem Ozlu <spammailatmayin@gmail.com>
2021-11-20 18:12:05 +0300

    fix wavefront

--- scripts/deploy-wavefront-tracing.sh
+++ scripts/deploy-wavefront-tracing.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
+TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $TKG_LAB_SCRIPTS/set-env.sh
 
 if [ ! $# -eq 1 ]; then
   echo "Must supply cluster name as args"
   exit 1
 fi
 CLUSTER_NAME=$1
-IAAS=$(yq r params.yaml iaas)
+IAAS=$(yq e .iaas $PARAMS_YAML)
 
 kubectl config use-context $CLUSTER_NAME-admin@$CLUSTER_NAME
 
-WAVEFRONT_API_KEY=$(yq r params.yaml wavefront.api-key)
-WAVEFRONT_URL=$(yq r params.yaml wavefront.url)
-WAVEFRONT_PREFIX=$(yq r params.yaml wavefront.cluster-name-prefix)
-WAVEFRONT_JAEGER_NAME=$(yq r params.yaml wavefront.jaeger-app-name-prefix)
-WORKLOAD_CLUSTER_NAME=$(yq r params.yaml workload-cluster.name)
+WAVEFRONT_API_KEY=$(yq e .wavefront.api-key $PARAMS_YAML)
+WAVEFRONT_URL=$(yq e .wavefront.url $PARAMS_YAML)
+WAVEFRONT_PREFIX=$(yq e .wavefront.cluster-name-prefix $PARAMS_YAML)
+WAVEFRONT_JAEGER_NAME=$(yq e .wavefront.jaeger-app-name-prefix $PARAMS_YAML)
+WORKLOAD_CLUSTER_NAME=$(yq e .workload-cluster.name $PARAMS_YAML)
 
 # Replace cluster tag in wf-preprocessor.yml
 sed -i "s/CLUSTERTAGNAME/$WAVEFRONT_PREFIX-$WORKLOAD_CLUSTER_NAME/g" wavefront/wf-preprocessor.yml
